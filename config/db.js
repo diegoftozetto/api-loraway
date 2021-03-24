eef8b8b51731eb18f3e4efdf61084e6d35c4eb19
Diego <diegotozetto@gmail.com>
2021-03-24 02:20:11 -0300

    Adiciona URI do Database MongoDB em produção

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: ""}
+    module.exports = {mongoURI: "mongodb+srv://admin:admin@cluster0.3ostd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"}
 }
 else{
     module.exports = {mongoURI: "mongodb://localhost/db-loraway"}
